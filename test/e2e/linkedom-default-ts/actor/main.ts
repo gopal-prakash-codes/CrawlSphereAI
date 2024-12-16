c2e6103dcbe18dd8142167d6ee0e1bb002a232a0
gopal-prakash-codes <gopalanprakash989@gmail.com>
2024-12-16 23:41:03 +0100

    continuing to build momentum with file updates

---
+++ test/e2e/linkedom-default-ts/actor/main.ts
@@ -0,0 +1,30 @@
+import assert from 'node:assert';
+
+import { LinkeDOMCrawler, Dataset } from '@crawlee/linkedom';
+import { Actor } from 'apify';
+
+if (process.env.STORAGE_IMPLEMENTATION === 'LOCAL') {
+    // @ts-ignore
+    await Actor.init({ storage: new (await import('@apify/storage-local')).ApifyStorageLocal() });
+} else {
+    await Actor.init();
+}
+
+const crawler = new LinkeDOMCrawler();
+
+crawler.router.addDefaultHandler(async ({ document, enqueueLinks, request, log }) => {
+    const { url } = request;
+    await enqueueLinks({
+        globs: ['https://crawlee.dev/docs/**'],
+    });
+
+    const pageTitle = document.title;
+    assert.notEqual(pageTitle, '');
+    log.info(`URL: ${url} TITLE: ${pageTitle}`);
+
+    await Dataset.pushData({ url, pageTitle });
+});
+
+await crawler.run(['https://crawlee.dev/docs/quick-start']);
+
+await Actor.exit({ exit: Actor.isAtHome() });
